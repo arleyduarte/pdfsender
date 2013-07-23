28b6a6d936a1f015f991518dbe75b150056b7155
unknown <arleymauricio@gmail.com>
2013-07-22 21:39:51 -0400

    My SQL connection

--- CargadorTuLlanta/CargadorTuLlanta/com/amdp/utils/MySQLConnector.cs
+++ CargadorTuLlanta/CargadorTuLlanta/com/amdp/utils/MySQLConnector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
+using log4net;
 
 
 namespace CargadorTuLlanta.com.amdp.utils
@@ -10,8 +11,7 @@ namespace CargadorTuLlanta.com.amdp.utils
     class MySQLConnector
     {
 
-
-
+        private static readonly ILog log = LogManager.GetLogger(typeof(MySQLConnector));
 
         public void connect()
         {
@@ -22,10 +22,17 @@ namespace CargadorTuLlanta.com.amdp.utils
             connectionString = "SERVER=" + Configuracion.Default.SERVER + ";Port="+Configuracion.Default.PORT+";" + "DATABASE=" +
              Configuracion.Default.DATABASE + ";" + "UID=" + Configuracion.Default.USERNAME + ";" + "PASSWORD=" + Configuracion.Default.PASSWORD + ";";
 
-        
-            MySqlConnection connection = new MySqlConnection(connectionString);
 
-            connection.Open();
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                connection.Open();
+            }catch(MySqlException ed){
+                log.Error("No se puedo realizar la conexión a la base de datos:"+connectionString+" "+ed);
+            }
+
+
         }
 
 
